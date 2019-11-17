9a13005daebebfe406e594f27b397b9e5c8f1c10
Sandra Xu <sxu5@cs.wellesley.edu>
2019-11-17 15:38:17 -0500

    ASdfasd

--- db.py
+++ db.py
@@ -22,12 +22,13 @@ def getAllPosts(conn):
 def getQueryAll(conn, query):
     curs = dbi.dictCursor(conn)
     curs.execute('''(select * from Posts where pname like %s 
-                                            or restaurant like %s)
+                                            or restaurant like %s
+                                            or location like %s)
                     union
                     (select Posts.pid, uid, pname, rating, price, review, restaurant, location, imgPath, time 
                                             from Posts inner join 
                                             (select pid from Tagpost inner join Tags 
                                             on Tags.tid = Tagpost.tid 
                                             where Tags.ttype like %s) as p 
-                                            on Posts.pid = p.pid)''', ['%'+query+'%', '%'+query+'%', query])
-    return curs.fetchall()
+                                            on Posts.pid = p.pid)''', ['%'+query+'%', '%'+query+'%', '%'+query+'%', query])
+    return curs.fetchall() # change to limit x offset y order by time
